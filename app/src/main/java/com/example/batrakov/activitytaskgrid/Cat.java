1810fefffe727768cd7c26796af709595545a967
Anton <phoenixed.451@gmail.com>
2017-10-02 15:09:39 +0300

    Almost Done

--- app/src/main/java/com/example/batrakov/activitytaskgrid/Cat.java
+++ app/src/main/java/com/example/batrakov/activitytaskgrid/Cat.java
@@ -6,24 +6,12 @@ import java.io.Serializable;
  * Created by batrakov on 28.09.17.
  */
 
-public class Cat implements Serializable {
+class Cat implements Serializable {
     private String mName;
     private String mBreed;
     private String mAge;
 
-    public Cat(){
-        mName = null;
-        mBreed = null;
-        mAge = null;
-    }
-
-    public Cat(Cat aCat){
-        this.mName = aCat.getName();
-        this.mBreed = aCat.getBreed();
-        this.mAge = aCat.getAge();
-    }
-
-    public Cat(String aName, String aBreed, String aAge){
+    Cat(String aName, String aBreed, String aAge) {
         mName = aName;
         mBreed = aBreed;
         mAge = aAge;
@@ -38,19 +26,11 @@ public class Cat implements Serializable {
         this.mName = aName;
     }
 
-    public String getBreed() {
+    String getBreed() {
         return mBreed;
     }
 
-    public void setBreed(String aBreed) {
-        this.mBreed = aBreed;
-    }
-
-    public String getAge() {
+    String getAge() {
         return mAge;
-    }
-
-    public void setAge(String aAge) {
-        this.mAge = aAge;
     }
 }
--- app/src/main/java/com/example/batrakov/activitytaskgrid/GridLayoutActivity.java
+++ app/src/main/java/com/example/batrakov/activitytaskgrid/GridLayoutActivity.java
@@ -1,74 +1,117 @@
 package com.example.batrakov.activitytaskgrid;
 
+
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class GridLayoutActivity extends AppCompatActivity {
 
-    ArrayList<Cat> mListData;
-    GridView mGridView;
-    GridAdapter mGridAdapter;
+    private ArrayList<Cat> mListData;
+    private RecyclerView mGridView;
+    private CatAdapter mGridAdapter;
+    private GridLayoutManager mManager;
     private static final String CAT_ARRAY = "cat array";
+    private static final String CAT_INDEX = "cat index";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.grid_layout);
-        mGridView = (GridView) findViewById(R.id.gridView);
+        mGridView = (RecyclerView) findViewById(R.id.gridView);
         mListData = new ArrayList<>();
-        if (mGridAdapter == null){
-            mGridAdapter = new GridAdapter(mListData);
+        if (mGridAdapter == null) {
+            mGridAdapter = new CatAdapter(mListData);
         }
+        mManager = new GridLayoutManager(this, 2);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            mManager.setSpanCount(2);
+        } else {
+            mManager.setSpanCount(3);
+        }
+
+        mGridView.setLayoutManager(mManager);
+        mGridView.setAdapter(mGridAdapter);
+        mGridAdapter.replaceData(mListData);
 
         ArrayList<String> stringArrayList;
-        if (this.getIntent().hasExtra(CAT_ARRAY)){
-
+        if (this.getIntent().hasExtra(CAT_ARRAY)) {
             stringArrayList = this.getIntent().getStringArrayListExtra(CAT_ARRAY);
-
-            for (int i = 0; i < stringArrayList.size(); i+=3) {
+            for (int i = 0; i < stringArrayList.size(); i += 3) {
                 mListData.add(new Cat(stringArrayList.get(i),
-                        stringArrayList.get(i+1),
-                        stringArrayList.get(i+2)));
+                        stringArrayList.get(i + 1),
+                        stringArrayList.get(i + 2)));
             }
-            mGridView.setAdapter(mGridAdapter);
-            mGridAdapter.replaceData(mListData);
         }
-                
     }
 
+    private class CatHolder extends RecyclerView.ViewHolder {
 
-    private class GridAdapter extends BaseAdapter{
+        private View mView;
+        private TextView mName;
+        private TextView mBreed;
+        private TextView mAge;
+        private Cat mCat;
 
-        private ArrayList<Cat> mListData;
-
-        GridAdapter(ArrayList<Cat> aListData){
-            mListData = aListData;
+        private CatHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mName = mView.findViewById(R.id.name);
+            mBreed = mView.findViewById(R.id.breed);
+            mAge = mView.findViewById(R.id.age);
         }
 
-        void replaceData(ArrayList<Cat> aListData){
-            mListData = aListData;
+        void bindView(Cat aCat) {
+            mCat = aCat;
+            mName.setText(mCat.getName());
+            mBreed.setText(mCat.getBreed());
+            mAge.setText(mCat.getAge());
+        }
+    }
+
+    private class CatAdapter extends RecyclerView.Adapter<CatHolder> {
+
+        private ArrayList<Cat> mList;
+
+        CatAdapter(ArrayList<Cat> aList) {
+            mList = aList;
+        }
+
+        void replaceData(ArrayList<Cat> aList) {
+            mList = aList;
             notifyDataSetChanged();
         }
 
         @Override
-        public int getCount() {
-            return mListData.size();
+        public CatHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View rowView = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item, parent, false);
+            rowView.setOnClickListener(mOnClickListener);
+            return new CatHolder(rowView);
         }
 
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                Intent intent = new Intent();
+                intent.putExtra(CAT_INDEX, mGridView.getChildLayoutPosition(view) + 1);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        };
+
         @Override
-        public Object getItem(int i) {
-            return mListData.get(i);
+        public void onBindViewHolder(CatHolder holder, int position) {
+            Cat cat = mList.get(position);
+            holder.bindView(cat);
         }
 
         @Override
@@ -77,21 +120,8 @@ public class GridLayoutActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View rowView = view;
-            if (rowView == null) {
-                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-                rowView = inflater.inflate(R.layout.grid_item, viewGroup, false);
-            }
-            final Cat cat = (Cat) getItem(i);
-
-            TextView name = rowView.findViewById(R.id.name);
-            name.setText(cat.getName());
-            TextView breed = rowView.findViewById(R.id.breed);
-            breed.setText(cat.getBreed());
-            TextView age = rowView.findViewById(R.id.age);
-            age.setText(cat.getAge());
-            return rowView;
+        public int getItemCount() {
+            return mList.size();
         }
     }
 }
